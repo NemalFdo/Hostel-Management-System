593c265f045a2e7364cd2aa816423fd80fb5d04d
NemalFdo <dinethnemal2002@gmail.com>
2024-05-02 21:39:28 +0530

    Final Project

---
+++ HousingPortalSDT/src/components/RejectBtn.jsx
@@ -0,0 +1,9 @@
+const RejectBtn = () => {
+    return(
+        <div>
+            <input type="button" value="Reject" className="w-[70px] h-[30px] bg-red-800 text-[13px] text-white rounded-[8px] cursor-pointer hover:opacity-[75%] active:bg-red-600 active:text-white ease-in-out"/>
+        </div>
+    );
+};
+
+export default RejectBtn;
